07e715649f47d6548c5064eb61a7752d558db73c
Stefan Bergh <it.stefan.bergh@gmail.com>
2015-06-11 10:24:58 +0200

    Dependent on specific version of stfnbrgh:nested-collection-helpers

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
-    api.use("stfnbrgh:nested-collection-helpers");
+    api.use("stfnbrgh:nested-collection-helpers@0.0.2");
     api.addFiles('collection-helpers-entities.js');
     api.export("CollectionHelpersEntities");
 });
