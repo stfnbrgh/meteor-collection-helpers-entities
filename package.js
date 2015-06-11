c0152033b9f1d03018f0cacca7bd47c3b64db4b3
Stefan Bergh <it.stefan.bergh@gmail.com>
2015-06-11 10:29:09 +0200

    Published to atmosphere

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'stfnbrgh:collection-helpers-entities',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: 'Register multiple helpers for an entity to attach on multiple collections or levels of the same document',
+    summary: 'Define helpers as entities and place them on your documents',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/stfnbrgh/meteor-collection-helpers-entities.git',
     // By default, Meteor will default to using README.md for documentation.
